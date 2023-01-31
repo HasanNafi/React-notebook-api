0fa8a7a58130f20ec28425609b0d29fb4e8e644f
Nafi <work.nafi@gmail.com>
2023-01-30 20:41:30 -0500

    added login credentitals

--- routes/auth.js
+++ routes/auth.js
@@ -10,11 +10,11 @@ const JWT_SECRET = "jwtsecret@1"
 
 // we will use post method otherwise user can get our data on their end which we dont want to show to our user
 // router.get('/',async (req,res)=>{
-  //create a user using : POST "/api/auth/createuser"
+  //create a user using : POST "/api/auth/createuser" .No login required
 router.post('/createuser',[
 
     body('name','Enter a valid name').isLength({ min: 3 }),
-     // email must be an email
+     // email must be an valid email
     body('email','Email must be valid and contain @.').isEmail(),
     // password must be at least 5 chars long
     body('password','Password must be at least 5 character').isLength({ min: 5 }),
@@ -54,9 +54,53 @@ router.post('/createuser',[
     //catch errors
     catch (error){
       console.error(error.message);
-      res.status(500).send("Error occured");
+      res.status(500).send("Internal server error");
     }
 
 })
 
+
+
+//=========================================     User login credentials      ======================================
+//authenticate user login using : POST "/api/auth/login" .No login required
+router.post('/login',[
+
+   // email must be an valid email
+  body('email','Email must be valid and contain @.').isEmail(),
+  body('password','Password can not be blank').exists(),
+
+  ],
+  async (req,res)=>{
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {email,password} = req.body;
+  try{
+    let user = await User.findOne({email}); //finding email id in the datatabse that email is exist in datatbase or not
+    if(!user){
+      return res.status(400).json({error:"Please login with correct credentials"});
+    }
+
+    const passwordCompare = await bcrypt.compare(password,user.password); //bcrypt is comparing hashes of user password to the passwords in database
+    if(!passwordCompare){
+      return res.status(400).json({error:"Please login with correct credentials"});
+    }
+    const payload = {
+      user:{
+        id:user.id
+      }
+    }
+    const authToken = jwt.sign(payload,JWT_SECRET); //creating token which we will send to our user to login next time with token
+    res.json({authToken})//sending token to user end
+
+  }//catch errors
+  catch (error){
+    console.error(error.message);
+    res.status(500).send("Internal server error");
+  }
+}
+)
 module.exports = router;
