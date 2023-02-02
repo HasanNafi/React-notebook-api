82ce3a25ba4473660cf3246545134a940eb63931
Nafi <work.nafi@gmail.com>
2023-02-02 11:53:33 -0500

    added update method for existing notes

--- routes/notes.js
+++ routes/notes.js
@@ -52,4 +52,42 @@ router.post(
   }
 );
 
+
+//==============================================================================================================
+//ROUTE:3 =>----------update an existing notes using : POST "/api/notes/updatenote/:id" .login required
+router.put("/updatenote/:id",fetchuser, async (req, res) => {
+      try {
+        const { title, description, tag } = req.body;
+        //create newNote object
+        const newNote = {};
+        if(title){
+          newNote.title = title
+        };
+        if(description){
+          newNote.description = description
+        };
+        if(tag){
+          newNote.tag = tag
+        };
+
+        //Find the note to be updated and update it
+        let note =await Note.findById(req.params.id);
+        if(!note){
+          return res.status(404).send("Not found")
+        }
+        if(note.user.toString() !== req.user.id){
+          return res.status(401).send("Not allowed")
+        }
+        note = await Note.findByIdAndUpdate(req.params.id, {$set : newNote}, {new:true})
+        res.json({note});
+
+
+      } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error");
+      }
+    }
+  );
+
+
 module.exports = router;
